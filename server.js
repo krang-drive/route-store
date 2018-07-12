7b5bf0fe8e3fd4b39339e9551f9a2246d28e6d08
NicholasCostanzo <costanzo.nicholas.j@gmail.com>
2018-07-12 19:44:58 -0400

    feat: add skeleton

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.listen(8080, () => console.log('App listening on port (krang) 8080!'));
+
+// KRANG
